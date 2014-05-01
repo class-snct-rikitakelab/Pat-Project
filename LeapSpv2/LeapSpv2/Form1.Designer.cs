9e586e46710c028bec9e965fa6046076c3b50c85
pingPat <patpat1993@gmail.com>
2014-05-01 17:14:50 +0700

    Add Connect Sphero button

--- LeapSpv2/LeapSpv2/Form1.Designer.cs
+++ LeapSpv2/LeapSpv2/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lconn = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -103,11 +104,21 @@
             this.lconn.Text = "Connected";
             this.lconn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lconn);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -131,6 +142,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox lconn;
+        private System.Windows.Forms.Button button1;
 
     }
 }
--- LeapSpv2/LeapSpv2/Program.cs
+++ LeapSpv2/LeapSpv2/Program.cs
@@ -14,6 +14,7 @@ namespace LeapSpv2
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
