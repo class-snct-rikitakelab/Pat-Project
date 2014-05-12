fc972ee55ee78b3419992c53eb2e60f30882701a
pingPat <patpat1993@gmail.com>
2014-05-12 10:52:30 +0700

    Sphero track hand + Random color when sphero move

--- LeapSpv2/LeapSpv2/LeapListener.cs
+++ LeapSpv2/LeapSpv2/LeapListener.cs
@@ -15,6 +15,11 @@ namespace LeapSpv2
         const int FramePause = 50000;
         private Form1 f;
         private Form2 f2;
+        private double baseX;
+        private double baseZ;
+        private int baseReset = 20;
+        private int baseReset2 = 0;
+        private int mode = 0;
         public void SetF(Form1 ff)
         {
             f = ff;
@@ -35,10 +40,81 @@ namespace LeapSpv2
             f.whenLDisconnect();
             f.lstatuscon = false;
         }
+        public double filter(double m)
+        {
+            if (m > 2)
+                return m - 2;
+            else if (m < -2)
+                return m + 2;
+            return 0;
+        }
         public override void OnFrame(Controller ctrl)
         {
             Frame frame = ctrl.Frame();
-            this.currentTime = frame.Timestamp;
+            if (frame.Hands.Count > 0 && f.sp != null)
+            {
+                Hand hand = frame.Hands[0];
+                int n = frame.Pointables.Count;
+                if (mode == 0 && n > 3)
+                {
+                    baseReset2 = 0;
+                }
+                else if (mode == 0 && n <= 2)
+                {
+                    if (baseReset2++ > 20)
+                        mode = 1;
+                    f.sp.SetRGBLEDOutput(255, 0, 0);
+                }
+                else if (mode == 1 && n >= 5)
+                {
+                    baseReset = 0;
+                    baseX = hand.PalmPosition.x;
+                    baseZ = hand.PalmPosition.z;
+                    mode = 2;
+                    f.sp.SetRGBLEDOutput(0, 255, 0);
+                }
+                else if (mode == 2 && n <= 2)
+                {
+                    if (baseReset++ > 20 || n == 0)
+                    {
+                        mode = 0;
+                        baseReset2 = 0;
+                        f.sp.Roll(0, 0, 1);
+                        f.sp.SetRGBLEDOutput(0, 0, 255);
+                    }
+                }
+                if (mode == 2)
+                {
+                    double x = filter(baseX - hand.PalmPosition.x);
+                    double z = -filter(baseX - hand.PalmPosition.z);
+                    double speed = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(z, 2));
+                    double deg = Math.Atan2(x, z) / Math.PI * 180;
+                    deg = (deg + 360) % 360;
+                    deg = deg + 90;
+                    f.sp.Roll((int)speed, (int)deg, 1);
+                    //change color
+                    int rNum, gNum, bNum;
+                    Random r = new Random();
+                    rNum = r.Next(0, 255);
+                    gNum = r.Next(0, 255);
+                    bNum = r.Next(0, 255);
+                    f.sp.SetRGBLEDOutput((byte)rNum, (byte)gNum, (byte)bNum);
+                }
+            }
+            else
+            {
+                if (baseReset++ > 20)
+                {
+                    if (mode == 2)
+                    {
+                        mode = 0;
+                        baseReset2 = 0;
+                        f.sp.Roll(0, 0, 1);
+                    }
+                }
+            }
+
+            /*this.currentTime = frame.Timestamp;
             this.timeChange = currentTime - previousTime;
             int i;
             if (frame.Hands.Count == 1 && f.sp != null)
@@ -120,10 +196,10 @@ namespace LeapSpv2
                                 f.sp.SetRGBLEDOutput((byte)rNum, (byte)gNum, (byte)bNum);
                                 f2.whenSPred();
                             }
-                            /*else if (gesture.Type == Gesture.GestureType.TYPESCREENTAP)
+                            else if (gesture.Type == Gesture.GestureType.TYPESCREENTAP)
                             {
                                 f.sp.Sleep();
-                            }*/
+                            }
                         }
                     }
                     previousTime = currentTime;
@@ -146,9 +222,9 @@ namespace LeapSpv2
                             }
                         }
                     }
-                    previousTime = currentTime;*/
+                    previousTime = currentTime;
                 }
-            }
+            }*/
         }
     }
 }
